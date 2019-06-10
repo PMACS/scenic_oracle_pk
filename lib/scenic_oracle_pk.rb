6de1a2b6d44ca0693ade0280c55a7a6a4b71e163
Walter Davis <waldavis@mail.med.upenn.edu>
2019-06-10 14:00:34 -0400

    move hierarchy

--- lib/scenic_oracle_pk.rb
+++ lib/scenic_oracle_pk.rb
@@ -3,19 +3,19 @@ require "scenic/command_recorder"
 
 module ScenicOraclePk
   class Error < StandardError; end
-  
-  module Scenic
-    module CommandRecorder
-      def create_pk_for_view(object:, key: 'id')
-        key_name = [object.to_s.split('.').last, key.to_s, 'pk'].join('_')
-        say_with_time do
-          suppress_messages do
-            execute <<-SQL
-            ALTER VIEW #{object} ADD CONSTRAINT #{key_name} PRIMARY KEY (#{key}) DISABLE
-            SQL
-          end
-          "Primary key #{key_name} created for #{object}."
+end
+
+module Scenic
+  module CommandRecorder
+    def create_pk_for_view(object:, key: 'id')
+      key_name = [object.to_s.split('.').last, key.to_s, 'pk'].join('_')
+      say_with_time do
+        suppress_messages do
+          execute <<-SQL
+          ALTER VIEW #{object} ADD CONSTRAINT #{key_name} PRIMARY KEY (#{key}) DISABLE
+          SQL
         end
+        "Primary key #{key_name} created for #{object}."
       end
     end
   end
