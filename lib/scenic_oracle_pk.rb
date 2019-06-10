b7e07642f4fb07d45430e447f19c3941eac7d786
Walter Davis <waldavis@mail.med.upenn.edu>
2019-06-10 15:05:09 -0400

    remove say

--- lib/scenic_oracle_pk.rb
+++ lib/scenic_oracle_pk.rb
@@ -9,13 +9,13 @@ module Scenic
   module Statements
     def create_pk_for_view(name, key = 'id')
       key_name = [name.to_s.split('.').last, key.to_s, 'pk'].join('_')
-      say_with_time %q[Primary key "#{key_name}" created for "#{name}".] do
-        suppress_messages do
+      # say_with_time %q[Primary key "#{key_name}" created for "#{name}".] do
+        # suppress_messages do
           execute <<-SQL
           ALTER VIEW #{quote_table_name(name)} ADD CONSTRAINT #{key_name} PRIMARY KEY (#{key}) DISABLE
           SQL
-        end
-      end
+        # end
+      # end
     end
   end
   
